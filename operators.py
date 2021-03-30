d708266d0a89a706f37e2b30376fd5b11df678a1
jjuarez7 <zS18021278@estudiantes.uv.mx>
2021-03-30 02:01:43 +0000

    add operators.py

---
+++ operators.py
@@ -0,0 +1,32 @@
+age1 = 9.0
+age2 = 20.0
+
+print " age1 = ", age1
+print " age2 = ", age2
+
+sumage = age1 + age2 
+print " suma (+) : ", sumage
+print " resta (-) : ", age1 - age2 
+print " mul (*) : ", age1 * age2
+print " div (/) : ", age1 / age2
+print " res (%) : ", age2 % age1
+
+message = "Today was a beautiful day"
+firstname = "jacqueline"
+lastname = "juarez"
+
+print message 
+print firstname
+print lastname
+
+print " sum strings (+) ", message  + " "  +  firstname  + " "  +  lastname 
+
+
+
+
+
+
+
+
+
+
