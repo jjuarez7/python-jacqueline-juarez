159cc2d5222d8baa63cfdf32bb09335a9538949d
jjuarez7 <zS18021278@estudiantes.uv.mx>
2021-03-30 00:50:36 +0000

    variables.py

---
+++ variables.py
@@ -0,0 +1,28 @@
+# variables.py
+# Author: Jacqueline Juarez Carrera
+# March 29,2021
+
+year = 2021;
+sentence = "My name is jjuarez"
+sarah, bob, mike = 16, 20, 25;
+name, age = "maria", 24;
+mary = fer = 20;
+PI = 3.1416;
+e = 2.71;
+
+# display values
+print "year = ", year
+print "sentence = ", sentence;
+print "\nsarah =", sarah, "\nbob =", bob, "\nmike =", mike;
+print "\n\t name = ", name;
+print "\n\t age = ", age;
+print mary, fer 
+print PI 
+print e
+
+
+
+
+
+
+
